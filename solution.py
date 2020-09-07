421a0096f428da58ee19c4fc107acd43ddf999cb
Syed Kamran Zaidi <skz227@nyu.edu>
2020-09-07 14:12:48 -0400

    Final_Submission

--- solution.py
+++ solution.py
@@ -16,17 +16,17 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "What is the MD5 hashing value to the following message:'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
-    elif question ==  "Is MD5 a secured hashing algorithm? - Yes/No":
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = 4
+        answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = 3
+        answer = 4
     return(answer)
 # Complete all the questions.
 
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
-    debug_question = "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number"
+    debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
